352237d55fd0a0b040f4dd47ad1115fb8a6692a5
HunCage <gkipeter@gmail.com>
2021-04-19 17:44:38 +0200

    author routes added

---
+++ routes/authors.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const author = require('../models/author')
+const router = express.Router()
+const Author = require('../models/author')
+
+
+// All Authors route
+router.get('/', async (req, res) => {
+  let serachOptions = {}
+  if (req.query.name != null && req.query.name !== '') {
+    serachOptions.name = new RegExp(req.query.name, 'i')
+  }
+  try {
+    const authors = await Author.find(serachOptions)
+    res.render('authors/index', {
+      authors: authors,
+      searchOptions: req.query
+    })
+  } catch {
+    res.redirect('/')
+  }
+})
+
+// New Author route
+router.get('/new', (req, res) => {
+  res.render('authors/new', {
+    author: new author()
+  })
+})
+
+// Create Athor route
+router.post('/', async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  })
+  try {
+    const newAuthor = await author.save()
+    // res.redirect(`authors/${newAuthor.id}`)
+    res.redirect(`authors`)
+
+  } catch {
+    res.render('authors/new', {
+      author: author,
+      errorMessage: 'Error creating Author'
+    })
+
+  }
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const expressLayouts = require('express-ejs-layouts')
 const mongoose = require('mongoose')
 
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -17,6 +18,11 @@ app.set('layout', 'layouts/layout')
 
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(express.urlencoded({
+  extended: false,
+  limit: '10mb'
+}));
+app.use(express.json());
 
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
@@ -27,5 +33,6 @@ db.on('error', error => console.error(error))
 db.once('open', () => console.info('Connected to mongoDB...'))
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000, console.log('Server is running...'))
